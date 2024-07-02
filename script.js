92c59217b093754e529345034765f83d8355b8c1
Kyomei25 <ueno.serina@kyomei.co>
2024-07-02 17:32:47 +0900

    Add initial HTML and CSS files

---
+++ script.js
@@ -0,0 +1,56 @@
+$(document).ready(function(){
+    $('.carousel').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: false,
+        adaptiveHeight: true
+    });
+});
+
+    // ナビゲーションのスムーズスクロール
+    $('nav a').on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800);
+        }
+    });
+
+    // ナビゲーションメニューのスライド機能
+    // ここにナビゲーションメニューのスライド機能を追加
+
+    $(document).ready(function(){
+        $('.product-gallery').slick({
+            dots: true,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 3000,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1,
+                    }
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+            ]
+        });
+    });
+
